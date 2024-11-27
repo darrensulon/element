e049cbca68a76c97eaeab72350e2725d35d2b4e8
darrensulon <103109806+darrensulon@users.noreply.github.com>
2024-11-27 21:23:23 +0200

    Delete util directory

--- util/model/ModelListener.java
+++
@@ -1,9 +0,0 @@
-package util.model;
-
-public interface ModelListener {
-
-	public void wasAdded(ModelEvent me);
-	public void wasChanged(ModelEvent me);
-	public void wasRemoved(ModelEvent me);
-	
-}
--- util/parser/IParser.java
+++
@@ -1,9 +0,0 @@
-package util.parser;
-
-import util.model.INamedObject;
-
-public interface IParser {
-
-	public INamedObject parse(int lineNr, String text, String ChosenAnalysis) throws ParseException;
-	
-}
